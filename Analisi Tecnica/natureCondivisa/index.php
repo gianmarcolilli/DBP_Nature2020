47d2e6f9495abdea3d212eb50ee0da3061b2f29d
Alberto <showmaster@live.it>
2020-06-20 16:19:39 +0200

    Aggiunta funzionalità home "leggi segnalazione" +  aggiustata struttura

--- Analisi Tecnica/natureCondivisa/index.php
+++ Analisi Tecnica/natureCondivisa/index.php
@@ -1,12 +1,10 @@
 <?php
-require_once('header.php');
-  ?>
+  require_once('header.php');
+	require_once('navbar.php');
+?>
 <body>
 
-<?php 
-	require_once('navbar.php');
-	?>
-  <!-- Masthead -->
+  <!-- Banner principale -->
   <header class="masthead text-white text-center">
     <div class="overlay"></div>
     <div class="container">
@@ -15,33 +13,23 @@ require_once('header.php');
           <h1 class="mb-5">Piattaforma di citizen science collaborativa per la
             gestione e classificazione della bio-diversità floristiche/faunistica</h1>
         </div>
-        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-          <!--<form>
-            <div class="form-row">
-              <div class="col-12 col-md-9 mb-2 mb-md-0">
-                <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">
-              </div>
-              <div class="col-12 col-md-3">
-                <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
-              </div>
-            </div>
-          </form>-->
-        </div>
       </div>
     </div>
   </header>
+    <!-- FINE Banner principale -->
 
 
   <?php
-  // QUESTO SARA' DA MODIFICARE PER IL CONTROLLO SESSIONE UTENTE
-    if (//isset($_SESSION['email']) && (!empty($_SESSION['email'])))
-    true){
+    // Controllo utente
+    if (isset( $_SESSION['email'] ) && ( !empty($_SESSION['email'] ) ) ) {
   ?>
 
   <!-- Lettura utenti -->
   <section class="bg-light text-center">
+    <br>
     <div class="container">
       <h2>Lettura utenti</h2>
+    <br>
     <?php
 
       $sql=$conn->query("SELECT nomeUtente, email FROM UTENTE");
@@ -59,31 +47,67 @@ require_once('header.php');
 
 
     </div>
+  <br><br>
   </section>
 
-  <?php
-      } else {
-        ?>
-        <br><br><br>
-        <div class="container" align="center"><h3>Per poter visualizzare, esegui il <a href="./login.html">login</a>!</h3></div>
-        <?php
-      }
-    ?>
-
   <!-- Image Showcases -->
-  <section class="showcase">
+  <section class="showcase side-padding">
+    <div align="center">
+      <h2>Effettua segnalazione</h2>
+    </div>
     <div class="container-fluid p-0">
-      <div class="row no-gutters">
-
-        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-1.jpg');"></div>
-        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-			<a  href="elencoSpecie.php"><h2>Elenco delle specie</h2></a>
-          <p class="lead mb-0">Cliccando sul link si visualizzerà l'elenco di tutte le specie faunistiche e floristiche. </p>
-			<a href="inserisciSpecieA.php"><p class="lead mb-0"><i class="fa fa-plus"></i>Inserisci nuova specie animale</p></a>
-			<a href="inserisciSpecieV.php"><p class="lead mb-0"><i class="fa fa-plus"></i>Inserisci nuova specie vegetale</p></a>
-			<a href="inserisciHabitat.php"><p class="lead mb-0"><i class="fa fa-plus"></i>Inserisci nuovo habitat</p></a>
+      <div class="row row-padding">
+        <div class="col-lg-6 col-md-6 col-sm-12 order-lg-1 my-auto" align="center">
+          <p><b>Lista segnalazioni effettuate</b></p>
+          <?php
+          $nomeUtente = $dataSegnalazione = $latitudineGPS = $longitudineGPS = "";
+          $sql=$conn->query(" SELECT nomeUtente, dataSegnalazione, latitudineGPS, longitudineGPS
+                              FROM segnalazione
+                            ");
+          echo "<table class=\"table-sm table-bordered table-hover\">
+                <thead>
+                <tr align=\"center\">
+                <th scope=\"col\">Nome utente</th>
+                <th scope=\"col\">Data</th>
+                <th scope=\"col\">Latitudine GPS</th>
+                <th scope=\"col\">Longitudine GPS</th>
+                </tr>
+                </thead>
+                <tbody>
+                ";
+          $count = mysqli_num_rows($sql);
+          if($count > 0){
+            while ($row = $sql->fetch_assoc()) {
+              unset($nomeUtente, $dataSegnalazione, $latitudineGPS, $longitudineGPS);
+              $nomeUtente = $row['nomeUtente'];
+              $dataSegnalazione = $row['dataSegnalazione'];
+              $latitudineGPS = $row['latitudineGPS'];
+              $longitudineGPS = $row['longitudineGPS'];
+              echo "<tr align=\"center\">
+              <th scope=\"row\">$nomeUtente</th>
+              <td>$dataSegnalazione</td>
+              <td>$latitudineGPS</td>
+              <td>$longitudineGPS</td>
+              </tr>
+              ";
+            }
+            echo "</tbody>
+            </table>
+            ";
+          } else {
+            echo "</tbody>
+            </table>
+            ";
+          }
+          ?>
+        </div> <!-- Fine colonna -->
+        <div class="col-lg-6 col-md-6 col-sm-12 order-lg-1 my-auto">
+          <p><b>Inserisci una segnalazione</b></p>
+          <p class="lead mb-0">Potrai visualizzare tutte le segnalazioni di avvistamento effettuate dagli utenti iscritti alla piattaforma.</p>
+          <a href="inserisciSegnalazione.php"><p class="lead mb-0"><i class="fa fa-plus"></i>Inserisci nuova segnalazione d'avvistamento</p></a>
         </div>
-      </div>
+      </div> <!-- Chiusura ROW -->
+
       <div class="row no-gutters">
         <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/bg-showcase-2.jpg');"></div>
         <div class="col-lg-6 my-auto showcase-text">
@@ -92,15 +116,22 @@ require_once('header.php');
 			<a href="inserisciRaccolta.php"><p class="lead mb-0"><i class="fa fa-plus"></i>Inserisci nuova campagna di raccolta fondi</p></a>
         </div>
       </div>
-      <div class="row no-gutters">
-        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('img/bg-showcase-3.jpg');"></div>
-        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
-          <a href="elencoSegnalazioni.php"><h2>Elenco delle segnalazioni di avvistamento</h2></a>
-          <p class="lead mb-0">Potrai visualizzare tutte le segnalazioni di avvistamento effettuate dagli utenti iscritti alla piattaforma.</p>
-			<a href="inserisciSegnalazione.php"><p class="lead mb-0"><i class="fa fa-plus"></i>Inserisci nuova segnalazione d'avvistamento</p></a>
+      <div class="row row-padding">
+        <div class="col-lg-6 col-md-6 col-sm-12 order-lg-1 my-auto">
+          <a href="elencoSpecie.php"><h2>Elenco delle specie</h2></a>
+          <p class="lead mb-0">Cliccando sul link si visualizzerà l'elenco di tutte le specie faunistiche e floristiche. </p>
+          <a href="inserisciSpecieA.php"><p class="lead mb-0"><i class="fa fa-plus"></i>Inserisci nuova specie animale</p></a>
+          <a href="inserisciSpecieV.php"><p class="lead mb-0"><i class="fa fa-plus"></i>Inserisci nuova specie vegetale</p></a>
+          <a href="inserisciHabitat.php"><p class="lead mb-0"><i class="fa fa-plus"></i>Inserisci nuovo habitat</p></a>
         </div>
+        <div class="col-lg-6 col-md-6 col-sm-12 order-lg-1 my-auto">
+          <a href="elencoSpecie.php"><h2>Elenco delle specie</h2></a>
+          <p class="lead mb-0">Cliccando sul link si visualizzerà l'elenco di tutte le specie faunistiche e floristiche. </p>
+          <a href="inserisciSpecieA.php"><p class="lead mb-0"><i class="fa fa-plus"></i>Inserisci nuova specie animale</p></a>
+          <a href="inserisciSpecieV.php"><p class="lead mb-0"><i class="fa fa-plus"></i>Inserisci nuova specie vegetale</p></a>
+          <a href="inserisciHabitat.php"><p class="lead mb-0"><i class="fa fa-plus"></i>Inserisci nuovo habitat</p></a></div>
       </div>
-		 <div class="row no-gutters">
+	    <div class="row no-gutters">
         <div class="col-lg-6 text-white showcase-img" style="background-image: url('img/bg-showcase-2.jpg');"></div>
         <div class="col-lg-6 my-auto showcase-text">
          <a href="elencoEscursioni.php"> <h2>Elenco delle escursioni</h2></a>
@@ -110,6 +141,17 @@ require_once('header.php');
       </div>
     </div>
   </section>
+
+
+    <?php
+        } else {
+          ?>
+          <br><br><br>
+          <div class="container" align="center"><h3>Per poter visualizzare, esegui il <a href="./login.html">login</a>!</h3></div>
+          <br><br><br>
+          <?php
+        }
+    ?>
 
   <!-- Testimonials -->
   <section class="testimonials text-center bg-light">
@@ -141,29 +183,7 @@ require_once('header.php');
     </div>
   </section>
 
-  <!-- Call to Action -->
-  <section class="call-to-action text-white text-center">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-xl-9 mx-auto">
-          <h2 class="mb-4">Ready to get started? Sign up now!</h2>
-        </div>
-        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
-          <form>
-            <div class="form-row">
-              <div class="col-12 col-md-9 mb-2 mb-md-0">
-                <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">
-              </div>
-              <div class="col-12 col-md-3">
-                <button type="submit" class="btn btn-block btn-lg btn-primary">Sign up!</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
-  </section>
+
 
   <!-- Footer -->
   <footer class="footer bg-light">
@@ -201,7 +221,7 @@ require_once('header.php');
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
+  <link rel="stylesheet" href="css/style.css">
 </body>
 
 </html>
